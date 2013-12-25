dbe0f186a3c63dcffa1f7174364b67e2b13274db
Jay Kanakiya <kanakiyajay@yahoo.com>
2013-12-25 12:46:12 +0530

    Initial Boilerplate

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/*!
+** Example App
+** Licensed under the Apache License v2.0
+** http://www.apache.org/licenses/LICENSE-2.0
+** Built by Jay Kanakiya ( @techiejayk )
+**/
+
+var App = angular.module("example",[]);
