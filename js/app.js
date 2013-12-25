5fbf390cc50c795a0737047d209c18e2ce437227
Jay Kanakiya <kanakiyajay@yahoo.com>
2013-12-25 13:20:03 +0530

    Create a basic model and view

--- js/app.js
+++ js/app.js
@@ -1,8 +1,24 @@
 /*!
-** Example App
+** Todo-Sortable-List Example App
 ** Licensed under the Apache License v2.0
 ** http://www.apache.org/licenses/LICENSE-2.0
 ** Built by Jay Kanakiya ( @techiejayk )
 **/
+"use strict";
 
-var App = angular.module("example",[]);
+var App = angular.module("todo",[]);
+
+App.controller("TodoCtrl",function  ($scope) {
+
+	$scope.todos = [
+								{ taskName : "Write an Angular js Tutorial for Todo-List" , isDone : false },
+								{ taskName : "Update jquer.in" , isDone : false },
+								{ taskName : "Create a brand-new Resume" , isDone : false }
+							];
+
+	$scope.addTodo = function  () {
+		$scope.todos.push({taskName : $scope.newTodo , isDone : false });
+		$scope.newTodo = "";
+	};
+
+});
